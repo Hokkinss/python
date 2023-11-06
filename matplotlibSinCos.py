3fe948c15ec19bfcf6ea4af2b3a5a34da0edd240
Hokkinss <146117952+Hokkinss@users.noreply.github.com>
2023-11-06 21:09:02 +0500

    Update matplotlibSinCos.py

--- matplotlibSinCos.py
+++ matplotlibSinCos.py
@@ -2,16 +2,16 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def func(x):
-    return np.sin(x) * np.cos(x)
+    return np.log(x) + np.cos(x)
 
-x = np.linspace(-2 * np.pi, 2 * np.pi, 1000)
+x = np.linspace(0.001, 10, 1000)
 
 y = func(x)
 
-plt.plot(x, y, label="sin(x) * cos(x)")
+plt.plot(x, y, label="log(x) + cos(x)")
 plt.xlabel("x")
 plt.ylabel("y")
-plt.title("График функции sin(x) * cos(x)")
+plt.title("График функции log(x) + cos(x)")
 plt.legend()
 plt.grid()
 plt.show()
